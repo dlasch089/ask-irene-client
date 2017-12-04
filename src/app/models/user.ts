f42b99ae3707c2ee25b72da45d64c3e47aa2ebd7
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-12-04 17:24:30 +0100

    progress: added models

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  id: string;
+  username: string;
+  password: string;
+  email: string;
+  image: string;
+  role: Array<string>;
+  favorites: Array<string>;
+  wishlist: Array<string>;
+}
