8e758c8660aa9b52b8e887190a2e81b528ff0314
Dominik Lasch <dl@exutec.de>
2017-12-10 20:23:14 +0100

    img-styling in list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Ask-Irene-App';
+  title = 'Ask Irene';
 
   loading = true;
   anon: boolean;
