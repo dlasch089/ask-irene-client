c28476440e597d8142543deabb6a652fb6614d01
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-12-13 12:57:31 +0100

    progress: random result working

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+
+import { SpotService} from '../../services/spot.service';
+import { Spot } from '../../models/spot';
 
 @Component({
   selector: 'app-page-home',
@@ -8,9 +11,19 @@ import { Router } from '@angular/router'
 })
 export class PageHomeComponent implements OnInit {
 
-  constructor() { }
+  randomSpot: any;
+
+  constructor(private spotService: SpotService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  handleRandom() {
+    this.spotService.getRandomSpot()
+      .subscribe((data) => {
+        this.randomSpot = data;
+        this.router.navigate([`/spots/${this.randomSpot._id}`])
+    });
   }
 
 }
